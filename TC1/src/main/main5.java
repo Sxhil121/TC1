ad06dfc4af2bca8048c8e8f33a200b02726330e7
Sahil Jadhav <Sahil Jadhav@LAPTOP-D35FPJO5>
2023-04-06 10:13:44 +0530

    github branch idea

--- TC1/src/main/main5.java
+++ TC1/src/main/main5.java
@@ -1,8 +1,5 @@
 package main;
 
 public class main5 {
-	public static void main(String[] args) {
-		System.out.println("So this version will be branched through github by comparing and pulling the request");
-	}
 
 }
