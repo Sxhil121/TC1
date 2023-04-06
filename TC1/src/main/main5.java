1daf0cd243c68bff57c2eb689c287f2d458414d0
Sahil Jadhav <Sahil Jadhav@LAPTOP-D35FPJO5>
2023-04-06 10:11:41 +0530

    Merge branch 'main' of https://github.com/Sxhil121/TC1.git into main

---
+++ TC1/src/main/main5.java
@@ -0,0 +1,8 @@
+package main;
+
+public class main5 {
+	public static void main(String[] args) {
+		System.out.println("So this version will be branched through github by comparing and pulling the request");
+	}
+
+}
